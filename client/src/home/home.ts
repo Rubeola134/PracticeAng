2f0c90476bc93718b8981896a0cfc519bba45a2b
Ruben Garcia <rubendanielgarciat@gmail.com>
2025-10-29 11:03:22 -0400

    onMouseMOve

--- client/src/home/home.ts
+++ client/src/home/home.ts
@@ -1,18 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Users } from "../users/users";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [Users],
+  imports: [Users, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
   protected hello: string = "Hello World"
   protected clicked: number = 0;
+  protected willShowBlock: boolean = true;
+  protected arr: number[] = [1,2,3,4,5]
+  protected contextClicked: boolean = false;
+  contextMenuInfo: any = {
+    pageX: 0,
+    pageY: 0,
+    willShow: false
+  };
+  tooltipInfo: any = {
+    pageX: 0,
+    pageY: 0,
+    willShow: false
+  };
+
+
 
   incrementClicked(){
     this.clicked += 1;
+  }
+
+  toggleContextMenu(showContextMenu: boolean, event: MouseEvent | null){
+    console.log(event);
+    if(event !== null){
+      event.preventDefault();
+      this.contextMenuInfo.pageX = event.pageX
+      this.contextMenuInfo.pageY =  event.pageY
+    }
+
+    this.contextMenuInfo.willShow = showContextMenu;
+  }
+
+  @HostListener("document:click")
+  closeContextMenu(){
+    setTimeout(() => {
+      if(!this.contextClicked) this.toggleContextMenu(false, null)
+    }, 10);
+
+  }
+
+  contextClick(){
+    this.contextClicked = true;
+    setTimeout(() =>{
+      this.contextClicked = false;
+    }, 20)
+  }
+
+  onMouseMove(inside: boolean, event: MouseEvent){
+    if(this.tooltipInfo.willShow !== inside){
+      this.tooltipInfo.willShow = inside;
+    }
+
+    this.tooltipInfo.pageX = event.pageX - 65;
+    this.tooltipInfo.pageY =  event.pageY + 15;
+    console.log(event);
   }
 
 }
